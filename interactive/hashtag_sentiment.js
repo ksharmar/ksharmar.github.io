643c76939cc99fd774ae5b497651e4b6b990329c
karishmaS <karishmasharma@Karishmas-MacBook-Air.local>
2020-03-27 09:52:04 -0700

    % sentiment hashtag

--- interactive/hashtag_sentiment.js
+++ interactive/hashtag_sentiment.js
@@ -1,22 +1,28 @@
 function draw_sentiment_socialdistancing() {
   // Define the chart to be drawn.
   var sentiment = [['Date', '+ Positive Sentiment', '- Negative Sentiment'],
- ['2020-03-09', 0.83, 0.17],
- ['2020-03-10', 0.64, 0.36],
- ['2020-03-11', 0.62, 0.38],
- ['2020-03-12', 0.62, 0.38],
- ['2020-03-13', 0.76, 0.24],
- ['2020-03-14', 0.7, 0.3],
- ['2020-03-15', 0.69, 0.31],
- ['2020-03-16', 0.69, 0.31],
- ['2020-03-17', 0.72, 0.28]];
+ ['2020-03-09', 83, 17],
+ ['2020-03-10', 64, 36],
+ ['2020-03-11', 62, 38],
+ ['2020-03-12', 62, 38],
+ ['2020-03-13', 76, 24],
+ ['2020-03-14', 7, 3],
+ ['2020-03-15', 69, 31],
+ ['2020-03-16', 69, 31],
+ ['2020-03-17', 72, 28]];
 
   var data = google.visualization.arrayToDataTable(sentiment);
   var options = {title: 'Sentiment % (2020-03-09 - 2020-03-17)',
                  isStacked:false,
                  chartArea:{left:60,top:30,width:"300px",height:"250px"},
                  height: 350,
-                 width: 500
+                 width: 500,
+                 vAxis: {
+                   viewWindow: {
+                       min: 0,
+                       max: 100
+                   },
+                 }
                };
   // Instantiate and draw the chart.
   var chart = new google.visualization.ColumnChart(document.getElementById('sentiment_socialdistancing_plot'));
@@ -28,21 +34,27 @@ google.charts.setOnLoadCallback(draw_sentiment_socialdistancing);
 function draw_sentiment_workfromhome() {
   // Define the chart to be drawn.
   var sentiment = [['Date', '+ Positive Sentiment', '- Negative Sentiment'],
- ['2020-03-09', 1.0, 0.0],
- ['2020-03-10', 0.33, 0.67],
- ['2020-03-11', 0.63, 0.37],
- ['2020-03-12', 0.78, 0.22],
- ['2020-03-13', 0.78, 0.22],
- ['2020-03-14', 0.86, 0.14],
- ['2020-03-15', 0.85, 0.15],
- ['2020-03-16', 0.76, 0.24],
- ['2020-03-17', 0.78, 0.22]];
+ ['2020-03-09', 100, 0],
+ ['2020-03-10', 33, 67],
+ ['2020-03-11', 63, 37],
+ ['2020-03-12', 78, 22],
+ ['2020-03-13', 78, 22],
+ ['2020-03-14', 86, 14],
+ ['2020-03-15', 85, 15],
+ ['2020-03-16', 76, 24],
+ ['2020-03-17', 78, 22]];
   var data = google.visualization.arrayToDataTable(sentiment);
   var options = {title: 'Sentiment % (2020-03-09 - 2020-03-17)',
                 isStacked:false,
                 chartArea:{left:60,top:30,width:"300px",height:"250px"},
                 height: 350,
-                width: 500
+                width: 500,
+                vAxis: {
+                  viewWindow: {
+                      min: 0,
+                      max: 100
+                  },
+                }
               };
   // Instantiate and draw the chart.
   var chart = new google.visualization.ColumnChart(document.getElementById('sentiment_workfromhome_plot'));
