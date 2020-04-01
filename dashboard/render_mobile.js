d1e5f84b3724094da995af4bce0325c6aa47442f
karishmaS <karishmasharma@Karishmas-MacBook-Air.local>
2020-04-01 08:03:41 -0700

    redirect

--- dashboard/render_mobile.js
+++
@@ -1,4 +0,0 @@
-var meta = document.createElement('meta');
-meta.name = "viewport";
-meta.content = "width=1280,initial-scale="+window.innerWidth/1280;
-document.getElementsByTagName('head')[0].appendChild(meta);
